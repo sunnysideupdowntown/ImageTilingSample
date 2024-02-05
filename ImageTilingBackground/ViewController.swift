16160a501707b51acf89287b19ceba9ef9d1a05c
Sunny <sunnysideupdowntown@gmail.com>
2024-02-05 21:53:04 +0900

    ScrollView 설정

--- ImageTilingBackground/ViewController.swift
+++ ImageTilingBackground/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollViewContentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
