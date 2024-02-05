5c562d5d8ad5d16052fcbd82231d9aeac8b6544b
Sunny <sunnysideupdowntown@gmail.com>
2024-02-05 22:23:48 +0900

    화면 크기에 이미지가 딱 맞게 타일링

--- ImageTilingBackground/ViewController.swift
+++ ImageTilingBackground/ViewController.swift
@@ -15,7 +15,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let image = UIImage(named: "brick-2997965_640.png")!
-        let color = UIColor(patternImage: image)
+        let aspectRatio = CGFloat(image.cgImage!.width) / CGFloat(image.cgImage!.height)
+        let size = CGSize(width: self.view.bounds.width, 
+                          height: self.view.bounds.width / aspectRatio)
+        let resizedImage = UIGraphicsImageRenderer(size: size).image { _ in
+            image.draw(in: CGRect(origin: .zero, size: size))
+        }
+        let color = UIColor(patternImage: resizedImage)
         self.scrollViewContentView.backgroundColor = color
     }
 }
